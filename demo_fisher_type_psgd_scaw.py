bb26c90ccedf150819d224175fd02923d1937e43
lixilinx <37510137+lixilinx@users.noreply.github.com>
2018-10-08 12:28:21 -0700

    Update demo_fisher_type_psgd_scaw.py

--- demo_fisher_type_psgd_scaw.py
+++ demo_fisher_type_psgd_scaw.py
@@ -1,3 +1,17 @@
+"""
+Let F = empirical Fisher information matrix. We consider P = F**(-0.5) vs P**2 = F**(-1)
+
+P = F**(-0.5): Adam and RMSProp use it with diagonal form. Their successes support this choice.
+
+P**2 = F**(-1): Amari's natural gradient theory support this choice.
+
+For this toy example, P**2 is better than P. Possible reasons: training data have enough diversity
+such that F is well conditioned; assuming prediction error as Gaussian is accurate here. 
+
+For many other tasks involving real word data, P might be better than P**2. Possible reasons: F is 
+ill conditioned as training data have low intrinsic dimensions while model is over parameterized; 
+mismatch between the assumed and real pdf models for data.    
+"""
 import matplotlib.pyplot as plt
 import torch
 from torch.autograd import grad
@@ -23,7 +37,8 @@ for num_iter in range(10000):
     delta = [torch.randn(W.shape) for W in Ws]
     with torch.no_grad():
         Qs = [psgd.update_precond_scaw(q[0], q[1], dw, dg + damping*dw) for (q, dw, dg) in zip(Qs, delta, grads)]
-        pre_grads = [psgd.precond_grad_scaw(q[0], q[1], g) for (q, g) in zip(Qs, grads)]
+        pre_grads = [psgd.precond_grad_scaw(q[0], q[1], g) for (q, g) in zip(Qs, grads)]# P*g: whitened gradients
+        pre_grads = [psgd.precond_grad_scaw(q[0], q[1], g) for (q, g) in zip(Qs, pre_grads)]# P*P*g: natural gradients
         grad_norm = torch.sqrt(sum([torch.sum(g*g) for g in pre_grads]))
         step_adjust = min(grad_norm_clip_thr/(grad_norm + 1.2e-38), 1.0)
         for i in range(len(Ws)):
