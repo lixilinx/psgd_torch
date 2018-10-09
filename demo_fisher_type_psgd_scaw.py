d4578a7eece5572bcc0c27856faf6a81185e3c1c
lixilinx <37510137+lixilinx@users.noreply.github.com>
2018-10-09 13:03:04 -0700

    Update demo_fisher_type_psgd_scaw.py

--- demo_fisher_type_psgd_scaw.py
+++ demo_fisher_type_psgd_scaw.py
@@ -1,16 +1,10 @@
 """
-Let F = empirical Fisher information matrix. We consider P = F**(-0.5) vs P**2 = F**(-1)
-
+Let F = empirical Fisher information matrix. We consider P = F**(-0.5) vs P**2 = F**(-1):
 P = F**(-0.5): Adam and RMSProp use it with diagonal form. Their successes support this choice.
+P**2 = F**(-1): Amari's natural gradient theory support this choice if F is close to the true Fisher matrix.
 
-P**2 = F**(-1): Amari's natural gradient theory support this choice.
-
-For this toy example, P**2 is better than P. Possible reasons: training data have enough diversity
-such that F is well conditioned; assuming prediction error as Gaussian is accurate here. 
-
-For many other tasks involving real word data, P might be better than P**2. Possible reasons: F is 
-ill conditioned as training data have low intrinsic dimensions while model is over parameterized; 
-mismatch between the assumed and real pdf models for data.    
+For this toy example, P**2 is better than P.
+For many other tasks involving real word data, P might be better than P**2.
 """
 import matplotlib.pyplot as plt
 import torch
@@ -38,7 +32,7 @@ for num_iter in range(10000):
     with torch.no_grad():
         Qs = [psgd.update_precond_scaw(q[0], q[1], dw, dg + damping*dw) for (q, dw, dg) in zip(Qs, delta, grads)]
         pre_grads = [psgd.precond_grad_scaw(q[0], q[1], g) for (q, g) in zip(Qs, grads)]# P*g: whitened gradients
-        pre_grads = [psgd.precond_grad_scaw(q[0], q[1], g) for (q, g) in zip(Qs, pre_grads)]# P*P*g: natural gradients
+        pre_grads = [psgd.precond_grad_scaw(q[0], q[1], g) for (q, g) in zip(Qs, pre_grads)]# P*P*g: natural gradients using empirical Fisher
         grad_norm = torch.sqrt(sum([torch.sum(g*g) for g in pre_grads]))
         step_adjust = min(grad_norm_clip_thr/(grad_norm + 1.2e-38), 1.0)
         for i in range(len(Ws)):
