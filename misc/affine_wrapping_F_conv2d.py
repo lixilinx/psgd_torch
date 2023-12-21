5e23cb901c55c6e5069af182e538df0c7d22972d
lixilinx <37510137+lixilinx@users.noreply.github.com>
2023-12-21 09:23:08 -0800

    wrapped affine preconditioners as a class

---
+++ misc/affine_wrapping_F_conv2d.py
@@ -0,0 +1,153 @@
+import sys
+
+import torch
+import torch.nn.functional as F
+from torchvision import datasets, transforms
+
+sys.path.append("..")
+import preconditioned_stochastic_gradient_descent as psgd
+
+device = torch.device("cuda")
+
+
+class AffineConv2d(torch.nn.Module):
+    """
+    Let's wrap function
+        torch.nn.functional.conv2d
+    as a class. The affine transform is
+        [vectorized(image patch), 1] @ W
+    """
+    def __init__(
+        self, in_channels, out_channels, kernel_size, stride=1, padding=0, has_bias=True
+    ):
+        super(AffineConv2d, self).__init__()
+        self.stride = stride
+        self.padding = padding
+        self.has_bias = has_bias
+        self.out_in_height_width = (out_channels, in_channels, kernel_size, kernel_size)
+
+        std = (in_channels * kernel_size**2) ** (-0.5)
+        w = torch.empty(out_channels, in_channels * kernel_size ** 2).normal_(std=std)
+        if has_bias:
+            b = torch.zeros(out_channels, 1)
+            self.weight = torch.nn.Parameter(torch.cat([w, b], dim=1))
+        else:
+            self.weight = torch.nn.Parameter(w)
+
+    def forward(self, x):
+        if self.has_bias:
+            return F.conv2d(
+                x,
+                self.weight[:, :-1].view(self.out_in_height_width),
+                bias=self.weight[:, -1],
+                stride=self.stride,
+                padding=self.padding,
+            )
+        else:
+            return F.conv2d(
+                x,
+                self.weight.view(self.out_in_height_width),
+                stride=self.stride,
+                padding=self.padding,
+            )
+
+
+class AffineLinear(torch.nn.Module):
+    """
+    A linear layer clearly is an affine transform
+    """
+    def __init__(self, in_features, out_features, has_bias=True):
+        super(AffineLinear, self).__init__()
+        self.has_bias = has_bias
+        w = torch.empty(in_features, out_features).normal_(std=in_features ** (-0.5))
+        if has_bias:
+            b = torch.zeros(1, out_features)
+            self.weight = torch.nn.Parameter(torch.cat([w, b]))
+        else:
+            self.weight = torch.nn.Parameter(w)
+
+    def forward(self, x):
+        if self.has_bias:
+            return x @ self.weight[:-1] + self.weight[-1]
+        else:
+            return x @ self.weight
+
+
+"""
+Now let's test our affine components
+"""
+train_loader = torch.utils.data.DataLoader(
+    datasets.MNIST(
+        "../data",
+        train=True,
+        download=True,
+        transform=transforms.Compose([transforms.ToTensor()]),
+    ),
+    batch_size=64,
+    shuffle=True,
+)
+test_loader = torch.utils.data.DataLoader(
+    datasets.MNIST(
+        "../data", train=False, transform=transforms.Compose([transforms.ToTensor()])
+    ),
+    batch_size=1000,
+    shuffle=False,
+)
+
+
+class LeNet5(torch.nn.Module):
+    def __init__(self):
+        super(LeNet5, self).__init__()
+        self.conv1 = AffineConv2d(1, 6, 5)
+        self.conv2 = AffineConv2d(6, 16, 5)
+        self.linear1 = AffineLinear(16 * 4 * 4, 120)
+        self.linear2 = AffineLinear(120, 84)
+        self.linear3 = AffineLinear(84, 10)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = F.relu(F.max_pool2d(x, 2))
+        x = self.conv2(x)
+        x = F.relu(F.max_pool2d(x, 2))
+        x = F.relu(self.linear1(x.view(-1, 16 * 4 * 4)))
+        x = F.relu(self.linear2(x))
+        return self.linear3(x)
+
+# lenet5 with affine transform components 
+lenet5 = LeNet5().to(device)
+
+
+@torch.jit.script
+def train_loss(data, target):
+    y = lenet5(data)
+    y = F.log_softmax(y, dim=1)
+    return F.nll_loss(y, target)
+
+
+def test_loss():
+    num_errs = 0
+    with torch.no_grad():
+        for data, target in test_loader:
+            data, target = data.to(device), target.to(device)
+
+            y = lenet5(data)
+            _, pred = torch.max(y, dim=1)
+            num_errs += torch.sum(pred != target)
+    return num_errs.item() / len(test_loader.dataset)
+
+
+opt = psgd.Affine(lenet5.parameters(), lr_params=0.1, grad_clip_max_norm=20.0)
+
+TrainLosses, best_test_loss = [], 1.0
+for epoch in range(10):
+    for _, (data, target) in enumerate(train_loader):
+        data, target = data.to(device), target.to(device)
+
+        def closure():
+            return train_loss(data, target)
+
+        TrainLosses.append(opt.step(closure).item())
+
+    best_test_loss = min(best_test_loss, test_loss())
+    opt.lr_params *= (0.01) ** (1 / 9)
+    print(f"Epoch: {epoch + 1}; best test classification error rate: {best_test_loss}")
---
+++ misc/affine_wrapping_VF_rnn_tanh.py
@@ -0,0 +1,205 @@
+import sys
+
+import matplotlib.pyplot as plt
+import numpy as np
+import torch
+
+sys.path.append("..")
+import preconditioned_stochastic_gradient_descent as psgd
+
+#torch.set_default_device(torch.device("cpu"))
+#torch.set_default_device(torch.device("cuda"))
+
+"""
+first let's check that our guess of the usage of function
+    torch._VF.rnn_tanh()
+is correct since there is no documument to follow.
+"""
+batch = 1
+length = 1
+input_size = 3
+hidden_size = 5
+num_layers = 2
+
+inputs = torch.randn(batch, length, input_size)  # (Batch, Length, Feature) format
+hx = torch.randn(num_layers, batch, hidden_size)
+
+# affine params, row major format 
+param_1st_layer = torch.randn(input_size + hidden_size + 1, hidden_size)
+param_2nd_layer = torch.randn(2 * hidden_size + 1, hidden_size)
+
+# prepare the param list for _VF.rnn_tanh to consume
+weights = [
+    param_1st_layer[: -hidden_size - 1].t().contiguous(), # this is annoying as torch RNN needs an extra transpose of weights 
+    param_1st_layer[-hidden_size - 1 : -1].t().contiguous(),
+    param_1st_layer[-1],
+    torch.zeros_like(param_1st_layer[-1]),
+    param_2nd_layer[: -hidden_size - 1].t().contiguous(),
+    param_2nd_layer[-hidden_size - 1 : -1].t().contiguous(),
+    param_2nd_layer[-1],
+    torch.zeros_like(param_2nd_layer[-1]),
+]
+
+# get _VF.rnn_tanh's result
+results = torch._VF.rnn_tanh(
+    inputs,
+    hx,
+    weights,
+    has_biases=True,
+    num_layers=num_layers,
+    dropout=0.0,
+    train=False,
+    bidirectional=False,
+    batch_first=True,
+)
+
+# get my own results
+out_1st_layer = torch.tanh(
+    inputs[0] @ weights[0].t() + hx[0] @ weights[1].t() + weights[2] + weights[3]
+)
+out_2nd_layer = torch.tanh(
+    out_1st_layer @ weights[4].t() + hx[1] @ weights[5].t() + weights[6] + weights[7]
+)
+print(f"the error should be close to 0: {out_2nd_layer - results[0]}")
+
+
+"""
+Now wrap our own RNN
+"""
+class AffineRNN(torch.nn.Module):
+    """
+    Class AffineRNN wraps function
+        torch._VF.rnn_tanh 
+    into a class.
+    I only consider the case with:
+        has_biases=True, bidirectional=False, batch_first=True, nonl=tanh
+        
+    The affine transform of each layer is 
+    
+                                        [    w_ih,  ]
+       [inputs, hidden states, 1]   @   [    w_hh,  ]
+                                        [    bias,  ]
+                                        
+    """
+    def __init__(self, input_size, hidden_size, num_layers=1, dropout=0.0):
+        super(AffineRNN, self).__init__()
+        self.num_layers = num_layers
+        self.hidden_size = hidden_size
+        self.dropout = dropout
+
+        def rnn_weight_initialization(input_size, hidden_size):
+            """
+            the weight matrix is store as
+            [   w_ih,
+                w_hh,
+                bias, ]
+            
+            The normal_ initialization might be better than uniform_ (keep the same variance)? 
+            """
+            w = torch.empty(input_size + hidden_size, hidden_size).normal_(
+                std=(3*(input_size + hidden_size)) ** (-0.5)
+            )
+            # w = torch.empty(input_size + hidden_size, hidden_size).uniform_(
+            #     -(input_size+hidden_size)**(-0.5), (input_size+hidden_size)**(-0.5)
+            # )
+            w[input_size:] = torch.qr(w[input_size:])[0]
+            b = torch.zeros(1, hidden_size)
+            return torch.cat([w, b])
+
+        self.param0 = torch.nn.Parameter(rnn_weight_initialization(input_size, hidden_size))
+        for i in range(num_layers - 1):
+            self.register_parameter(
+                f"param{i+1}",
+                torch.nn.Parameter(rnn_weight_initialization(hidden_size, hidden_size)),
+            )
+
+    def forward(self, inputs, hx):
+        # prepare weights for _VF.rnn_tanh
+        weights = []
+        for i in range(self.num_layers):
+            p = self.get_parameter(f"param{i}")
+            weights.extend(
+                [
+                    p[: -self.hidden_size - 1].t().contiguous(), # ok, we can save this .t().contiguous() if everyone follows row major convention 
+                    p[-self.hidden_size - 1 : -1].t().contiguous(),
+                    p[-1],
+                    torch.zeros_like(p[-1]),
+                ]
+            )
+        # call _VF.rnn_tanh to return the (outputs, hidden states)
+        return torch._VF.rnn_tanh(
+            inputs,
+            hx,
+            weights,
+            has_biases=True,
+            num_layers=self.num_layers,
+            dropout=self.dropout,
+            train=self.training, # this self.training is inherited from torch.nn.Module 
+            bidirectional=False,
+            batch_first=True,
+        )
+
+# let's test our wrapped RNN
+batch, length, input_size, hidden_size, num_layers = 4, 6, 3, 5, 2
+rnn = AffineRNN(input_size, hidden_size, num_layers=num_layers)
+inputs = torch.randn(batch, length, input_size)  
+hx = torch.randn(num_layers, batch, hidden_size)
+results = rnn(inputs, hx)
+
+"""
+Lastly, let's solve the delayed XOR problem with the Affine RNN and preconditioner
+"""
+device = hx.device
+batch_size, seq_len = (128, 16,) 
+dim_in, dim_hidden, dim_out = (2, 30, 1,)
+
+def generate_train_data():
+    x = np.zeros([batch_size, seq_len, dim_in], dtype=np.float32)
+    y = np.zeros([batch_size, dim_out], dtype=np.float32)
+    for i in range(batch_size):
+        x[i, :, 0] = np.random.choice([-1.0, 1.0], seq_len)
+
+        i1 = int(np.floor(np.random.rand() * 0.1 * seq_len))
+        i2 = int(np.floor(np.random.rand() * 0.4 * seq_len + 0.1 * seq_len))
+        x[i, i1, 1] = 1.0
+        x[i, i2, 1] = 1.0
+        if x[i, i1, 0] == x[i, i2, 0]:  # XOR
+            y[i] = -1.0  # lable 0
+        else:
+            y[i] = 1.0  # lable 1
+
+    return [torch.from_numpy(x).to(device), torch.from_numpy(y).to(device)]
+
+class Model(torch.nn.Module):
+    def __init__(self):
+        super(Model, self).__init__()
+        self.rnn = AffineRNN(dim_in, dim_hidden)
+        self.linear_wb = torch.nn.Parameter(0.1 * torch.randn(dim_hidden + 1, dim_out))
+
+    def forward(self, xs):
+        _, h = self.rnn(xs, torch.zeros(1, batch_size, dim_hidden))
+        return h[0] @ self.linear_wb[:-1] + self.linear_wb[-1]
+
+model = Model()
+
+opt = psgd.Affine(
+    model.parameters(), grad_clip_max_norm=1.0, exact_hessian_vector_product=False
+)
+
+def train_loss(xy_pair):  # logistic loss
+    return -torch.mean(torch.log(torch.sigmoid(xy_pair[1] * model(xy_pair[0]))))
+
+Losses = []
+for num_iter in range(100000):
+    train_data = generate_train_data()
+
+    def closure():
+        return train_loss(train_data) 
+
+    Losses.append(opt.step(closure).item())
+    print(f"Iteration: {num_iter + 1}; loss: {Losses[-1]}")
+
+    if Losses[-1] < 0.1:
+        print("Deemed to be successful and ends training")
+        break
+plt.plot(Losses)
