057fcefdc2d9cf34faca4f8c44b318a30ca10c99
lixilinx <37510137+lixilinx@users.noreply.github.com>
2025-04-20 21:35:21 -0700

    demo one side whitening with psgd
    Upgrade to matmul only kron psgd.

--- misc/psgd_one_side_whitening_like_muon.py
+++ misc/psgd_one_side_whitening_like_muon.py
@@ -1,20 +1,23 @@
 import sys 
 import torch
 sys.path.append("..")
-from preconditioned_stochastic_gradient_descent import norm_lower_bound
 
+from psgd import norm_lower_bound_herm
 
-def muon_style_psgd(G, GhG, Q, lr_preconditioner=0.05, preconditioner_update_probability=1.0):
+def muon_style_psgd(G, GhG, Q, lr_preconditioner=0.1, preconditioner_update_probability=1.0):
     """
-    The muon style PSGD fits the preconditioner in Lie group kron(Q2=Q, Q1=I) for a tall matrix gradient G, 
-    and group kron(Q2=I, Q1=Q) for a short matrix gradient G. 
-    It's a special case of the PSGD Kron whitening preconditioner. 
-    I further 
-        1) integrate out the auxiliary variable V as keeping it saves nothing here;
-        2) keep the EMA of E[G.H @ G] as GhG (assuming G is a tall matrix) is all we need to fit Q. 
-    The difference with muon is that muon does instantaneous whitening, while PSGD does ensemble whitening. 
-    
-    This function returns whitened gradient, updated GhG and preconditioner Q. 
+    Muon style PSGD whitens gradient/moment along the smallest dim. 
+    Args:
+        G: 2D gradient tensor;
+        GhG: EMA of G.H @ G;
+        Q: factor of preconditioner P = Q.H @ Q;
+        lr_preconditioner: learning rate of preconditioner; 
+        preconditioner_update_probability: probability for update Q. 
+        
+    Outputs:
+        precondG: preconditioned gradient;
+        GhG: updated EMA of G.H @ G;
+        Q: (optionally) updated factor of preconditioner P = Q.H @ Q. 
     """
     m, n = G.shape # assert G.dim() == 2 implicitly
     transposed = False
@@ -23,19 +26,20 @@ def muon_style_psgd(G, GhG, Q, lr_preconditioner=0.05, preconditioner_update_pro
         m, n = n, m
         transposed = True
         
-    # better to set initial states GhG and Q to None 
+    # init GhG and Q on the fly if they are None. 
     if GhG is None:
         GhG = G.H @ G
     else:
         GhG = 0.9 * GhG + 0.1 * G.H @ G
     if Q is None:
-        Q = torch.eye(n, device=G.device) * (n/torch.trace(GhG))**0.25
+        Q = torch.eye(n, device=G.device, dtype=G.dtype) * (n/torch.trace(GhG))**0.25
         
     precondG = G @ Q.H @ Q
     if torch.rand([]) < preconditioner_update_probability:
-        PGhGP = precondG.H @ precondG
-        lr = lr_preconditioner/(norm_lower_bound(PGhGP) + 1)
-        Q = (1 + lr) * Q - lr * PGhGP @ Q  
+        term1 = precondG @ Q.H
+        term1, term2 = term1.H @ term1, Q @ Q.H
+        lr = lr_preconditioner/norm_lower_bound_herm(term1 + term2)
+        Q = Q - lr * (term1 - term2) @ Q 
     
     if transposed:
         return (precondG.H, GhG, Q)
@@ -44,24 +48,37 @@ def muon_style_psgd(G, GhG, Q, lr_preconditioner=0.05, preconditioner_update_pro
     
 
 if __name__ == "__main__":
-    # test muon style psgd: tall real matrix
-    GhG = None # EMA of G.T @ G for tall G 
-    Q = None # preconditioner Q 
-    R = 0.0 # covariance matrix of whitened G
+    # test with tall real matrix
+    GhG = None  # EMA of G.T @ G 
+    Q = None    # preconditioner Q 
+    R = 0.0     # covariance matrix of whitened G
     A = torch.randn(4, 4)
     for _ in range(1000):
         G = A @ torch.randn(4, 8)
         whitenedG, GhG, Q = muon_style_psgd(G, GhG, Q)
         R = 0.9*R + 0.1*whitenedG @ whitenedG.T
-    print(f"Covariance matrix of whitened G (should be close to I): \n {R.numpy()}")
+    print(f"Covariance matrix of whitened G (should be close to I): \n {R.numpy()}\n")
     
-    # test muon style psgd: short complex matrix
-    GhG = None # EMA of G.H @ G for tall G
+    # test with short complex matrix
+    GhG = None  
     Q = None
-    R = 0.0 # covariance matrix of whitened G
+    R = 0.0  
     A = torch.randn(4, 4, dtype=torch.complex64)  
     for _ in range(1000):
         G = torch.randn(8, 4, dtype=torch.complex64) @ A
         whitenedG, GhG, Q = muon_style_psgd(G, GhG, Q)
         R = 0.9*R + 0.1*whitenedG.H @ whitenedG
     print(f"|Covariance matrix| of whitened G (should be close to I): \n {torch.abs(R).numpy()}")
+    
+    # test with ill-conditioned matrix
+    import scipy 
+    import matplotlib.pyplot as plt
+    G = torch.from_numpy(scipy.linalg.hilbert(64))
+    G.diagonal().add_(1e-7)
+    GhG = None 
+    Q = None
+    R = 0.0
+    for _ in range(32):
+        whitenedG, GhG, Q = muon_style_psgd(G, GhG, Q, lr_preconditioner=0.5)
+    plt.imshow(whitenedG)
+    plt.title("Whitened gradient should be close to I for PSD gradient")
