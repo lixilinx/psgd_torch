9164b30b9d3f7bed8c63c47a55fb53adc8dac9a0
lixilinx <37510137+lixilinx@users.noreply.github.com>
2025-12-05 21:33:50 -0800

    torch.optim wrapping for DDP: improved logic
    1, refactored and improved the logic.
    
    2, support state resync if matmul is not deterministic (resync frequency is problem specific, but should be very low in general).

--- wrapped_as_torch_optimizer_for_ddp.py
+++ wrapped_as_torch_optimizer_for_ddp.py
@@ -1,10 +1,7 @@
-import os
-
+import torch
 import psgd
 
-import torch
-from torch.nn.parallel import DistributedDataParallel as DDP
-
+# we can have some settings to force GPU matmul determinstic here, but it could slow down the code. 
 
 class WhitenMomentumNS4(torch.optim.Optimizer):
     """
@@ -18,13 +15,13 @@ class WhitenMomentumNS4(torch.optim.Optimizer):
             preconditioner_max_size=float("inf"), 
             preconditioner_max_skew=1.0, # 0.0 => all diagonal Q; inf => all dense Q
             preconditioner_init_scale=1.0, # P0 = preconditioner_init_scale^2 * I; smaller is safer
-            lr_params=0.0003, # roughly sqrt((1+momentum)/(1-momentum)) times smaller than Adam's lr  
+            lr_params=3e-4, # roughly sqrt((1+momentum)/(1-momentum)) times smaller than Adam's lr  
             lr_preconditioner=0.1, 
             betaL=0.9, 
             damping=1e-9, # roughly the eps=1e-8 in Adam 
-            momentum=0.9, # momentum is not always helpful!
+            momentum=0.9, # momentum is not always helpful; reduce it if so
             weight_decay=0.0, # L2 regularization, not decoupled wd 
-            grad_clip_max_amp=2.0, # unusual to have amplitude>>1.0; clip if too big 
+            grad_clip_max_amp=2.0, # rare to have amplitude>>1.0 after whitening; clip if too big 
             preconditioner_update_probability=0.1, # successive momentum are correlated, no need to update every step 
     ):
         defaults = {
@@ -47,14 +44,12 @@ class WhitenMomentumNS4(torch.optim.Optimizer):
         self.precond_grad = psgd.precond_grad_kron
 
         self.is_distributed = torch.distributed.is_available() and torch.distributed.is_initialized()
-        if self.is_distributed:
-            # we assume it's distributed; important to let all the copies have synced rng states
-            self.device = self.param_groups[0]["params"][0].device
-            state = torch.get_rng_state().to(self.device) # assume nccl backend
+        if self.is_distributed: # if True, assume distributed GPU training; important to sync the rng states
+            state = torch.get_rng_state().cuda() # assume nccl backend; use gloo if there are complex parameters
             torch.distributed.broadcast(state, src=0)
             self.cpu_rng_state = state.cpu()
 
-            state = torch.cuda.get_rng_state().to(self.device)
+            state = torch.cuda.get_rng_state().cuda()
             torch.distributed.broadcast(state, src=0)
             self.cuda_rng_state = state.cpu()
 
@@ -73,7 +68,7 @@ class WhitenMomentumNS4(torch.optim.Optimizer):
                 grad = p.grad 
 
                 wd = group["weight_decay"]
-                if wd > 0.0:
+                if wd > 0.0: # here is the classic wd; just p.mul_(1 - wd * lr_params) for decoupled wd
                     grad = grad.add(p, alpha=wd)
 
                 grad = grad.squeeze() # squeeze out dim=1
@@ -101,6 +96,17 @@ class WhitenMomentumNS4(torch.optim.Optimizer):
                 amp = torch.sqrt(torch.real(torch.mean(h * h.conj())))
                 p.subtract_(group["lr_params"]/torch.clamp(amp/group["grad_clip_max_amp"], min=1.0) * h.view_as(p))
 
+                # resync states occasionally if matmul is not determinstic; 
+                # no resync if matmul is determinstic or all preconditioners are diagonal    
+                if self.is_distributed and (state["step"] % 10000 == 0):
+                    torch.distributed.broadcast(p, src=0)
+                    torch.distributed.broadcast(state["ema"], src=0)
+                    Q, L = state["QL"]
+                    for q in Q:
+                        torch.distributed.broadcast(q, src=0)
+                    for ell in L:
+                        torch.distributed.broadcast(ell, src=0)
+
         if self.is_distributed: # save internal rng states; recover external rng states 
             self.cpu_rng_state = torch.get_rng_state()
             self.cuda_rng_state = torch.cuda.get_rng_state()
@@ -108,36 +114,35 @@ class WhitenMomentumNS4(torch.optim.Optimizer):
             torch.cuda.set_rng_state(external_cuda_rng_state)
 
 
-class ToyModel(torch.nn.Module):
-    def __init__(self):
-        super(ToyModel, self).__init__()
-        self.w = torch.nn.Parameter(torch.randn(1,2,3,4))
+if __name__ == "__main__":
+    # toy demo for verification  
+    import os
+    from torch.nn.parallel import DistributedDataParallel as DDP
 
-    def forward(self, x):
-        y = (self.w - 1) * x
-        return torch.real(torch.mean(y * y.conj()))
-
-
-def demo():
     local_rank = int(os.environ.get("LOCAL_RANK", "0"))
     rank = int(os.environ.get("RANK", "0")) # rank!=local_rank if #(nodes)>1
     device = torch.device(f"cuda:{local_rank}")
     torch.cuda.set_device(device)
     torch.distributed.init_process_group(backend="nccl") # use gloo for complex tensors; nccl does not support complex tensors! 
 
+    class ToyModel(torch.nn.Module):
+        def __init__(self):
+            super(ToyModel, self).__init__()
+            self.w = torch.nn.Parameter(torch.randn(1,2,3,4))
+
+        def forward(self, x):
+            y = (self.w - torch.rand_like(self.w)) * x
+            return torch.real(torch.mean(y * y.conj()))
+
     model = DDP(ToyModel().to(device), device_ids=[local_rank])
-    opt = WhitenMomentumNS4(model.parameters(), weight_decay=1e-6)
+    opt = WhitenMomentumNS4(model.parameters())
     for _ in range(1000):
         opt.zero_grad()
         loss = model(torch.randn_like(model.module.w))
         loss.backward()
         opt.step()
 
-    # let's check its correctness
+    # let's verify the wrapping  
     print(f"rank {rank} got loss {loss}") # different GPU should have different loss 
     print(f"rank {rank} got model weights {model.module.w}") # model weights should be close (no bitwise match guarantee)
     print(f"""rank {rank} got preconditioner states {opt.state[model.module.w]["QL"]}""") # close but no bitwise match guarantee
-
-
-if __name__ == "__main__":
-    demo()
