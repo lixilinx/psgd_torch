1abf96f5b956646e5610c9c0c0cd72cf30e9188a
lixilinx <37510137+lixilinx@users.noreply.github.com>
2025-12-07 18:47:08 -0800

    torch.optim DDP wrapping: improved logic
    1, state resync frequency setting is exposed. 
    
    2, if momentum=0, save the EMA buffer.

--- wrapped_as_torch_optimizer_for_ddp.py
+++ wrapped_as_torch_optimizer_for_ddp.py
@@ -1,12 +1,12 @@
 import torch
 import psgd
 
-# we can have some settings to force GPU matmul determinstic here, but it could slow down the code. 
+# we can have some settings to force GPU matmul determinstic here, but they slow down the training  
 
 class WhitenMomentumNS4(torch.optim.Optimizer):
     """
     Whiten momentum with online Newton-Schulz iteration for inverse 4th root of momentum correlation matrix. 
-    Largely corresponds to psgd.KronWhiten with dQ="Q0.5EQ1.5" and whiten_grad=False.
+    Largely corresponds to psgd.KronWhiten with dQ="Q0.5EQ1.5" and whiten_grad=False. Wrapped for DDP. 
     """
 
     def __init__(
@@ -15,14 +15,15 @@ class WhitenMomentumNS4(torch.optim.Optimizer):
             preconditioner_max_size=float("inf"), 
             preconditioner_max_skew=1.0, # 0.0 => all diagonal Q; inf => all dense Q
             preconditioner_init_scale=1.0, # P0 = preconditioner_init_scale^2 * I; smaller is safer
-            lr_params=3e-4, # roughly sqrt((1+momentum)/(1-momentum)) times smaller than Adam's lr  
+            lr_params=3e-4, # roughly sqrt((1+momentum)/(1-momentum)) times smaller than Adam, i.e., sqrt((1-momentum)/(1+momentum)) * 1e-3   
             lr_preconditioner=0.1, 
             betaL=0.9, 
             damping=1e-9, # roughly the eps=1e-8 in Adam 
-            momentum=0.9, # momentum is not always helpful; reduce it if so
+            momentum=0.9, # if momentum is not helpful, reduce or zero it and increase lr_params
             weight_decay=0.0, # L2 regularization, not decoupled wd 
             grad_clip_max_amp=2.0, # rare to have amplitude>>1.0 after whitening; clip if too big 
-            preconditioner_update_probability=0.1, # successive momentum are correlated, no need to update every step 
+            preconditioner_update_probability=0.1, # recommend to start from 1.0, and quickly anneal to 0.1 or 0.01 
+            resync_every=100000, # nondeterminstic matmul diverge states for PSGD like matrix preconditioner; resync every # steps 
     ):
         defaults = {
             "preconditioner_max_size": preconditioner_max_size, 
@@ -36,6 +37,7 @@ class WhitenMomentumNS4(torch.optim.Optimizer):
             "weight_decay": weight_decay,
             "grad_clip_max_amp": grad_clip_max_amp, 
             "preconditioner_update_probability": preconditioner_update_probability,
+            "resync_every": resync_every,
         }
         super().__init__(params, defaults)
 
@@ -44,7 +46,7 @@ class WhitenMomentumNS4(torch.optim.Optimizer):
         self.precond_grad = psgd.precond_grad_kron
 
         self.is_distributed = torch.distributed.is_available() and torch.distributed.is_initialized()
-        if self.is_distributed: # if True, assume distributed GPU training; important to sync the rng states
+        if self.is_distributed: # if True, assume multi-GPU DDP training; important to sync the rng states
             state = torch.get_rng_state().cuda() # assume nccl backend; use gloo if there are complex parameters
             torch.distributed.broadcast(state, src=0)
             self.cpu_rng_state = state.cpu()
@@ -62,6 +64,7 @@ class WhitenMomentumNS4(torch.optim.Optimizer):
             torch.cuda.set_rng_state(self.cuda_rng_state)
 
         for group in self.param_groups:
+            update_preconditioner = torch.rand([]) < group["preconditioner_update_probability"]
             for p in group["params"]:
                 if p.grad is None:
                     continue
@@ -71,7 +74,7 @@ class WhitenMomentumNS4(torch.optim.Optimizer):
                 if wd > 0.0: # here is the classic wd; just p.mul_(1 - wd * lr_params) for decoupled wd
                     grad = grad.add(p, alpha=wd)
 
-                grad = grad.squeeze() # squeeze out dim=1
+                grad = grad.squeeze() # squeeze out dim=1; also good to merge small dims if possible 
 
                 state = self.state[p]
                 if len(state) == 0: # intialization
@@ -81,26 +84,32 @@ class WhitenMomentumNS4(torch.optim.Optimizer):
                                                                  max_skew=group["preconditioner_max_skew"], 
                                                                  dQ=self.dQ)
                     state["step"] = 0
-                    state["ema"] = torch.zeros_like(grad) # exp moving avg of grad as momentum 
+                    if group["momentum"] > 0.0:
+                        state["ema"] = torch.zeros_like(grad) # exp moving avg of grad as momentum 
 
                 t = state["step"]
-                beta = min(t/(t + 1), group["momentum"])
-                state["ema"].mul_(beta).add_(grad, alpha=1 - beta)
+                momentum = group["momentum"]
+                if momentum > 0.0:
+                    beta = min(t/(t + 1), momentum)
+                    g = state["ema"]
+                    g.mul_(beta).add_(grad, alpha=1 - beta)
+                else:
+                    g = grad
                 state["step"] += 1
 
-                if torch.rand([]) < group["preconditioner_update_probability"]:
-                    self.update_precond(state["QL"], state["exprs"], state["ema"], 
+                if update_preconditioner:
+                    self.update_precond(state["QL"], state["exprs"], g, 
                                         lr=group["lr_preconditioner"], betaL=group["betaL"], damping=group["damping"])
                     
-                h = self.precond_grad(state["QL"], state["exprs"], state["ema"]) # preconditioned momentum 
-                amp = torch.sqrt(torch.real(torch.mean(h * h.conj())))
+                h = self.precond_grad(state["QL"], state["exprs"], g) # preconditioned momentum 
+                amp = torch.sqrt(torch.real(torch.mean(h * h.conj()))) # psgd supports complex tensors; note that nccl does not
                 p.subtract_(group["lr_params"]/torch.clamp(amp/group["grad_clip_max_amp"], min=1.0) * h.view_as(p))
 
-                # resync states occasionally if matmul is not determinstic; 
-                # no resync if matmul is determinstic or all preconditioners are diagonal    
-                if self.is_distributed and (state["step"] % 10000 == 0):
+                # resync states occasionally if matmul is not determinstic and preconditioner_max_skew>0.0
+                if self.is_distributed and (state["step"] % group["resync_every"] == 0):
                     torch.distributed.broadcast(p, src=0)
-                    torch.distributed.broadcast(state["ema"], src=0)
+                    if group["momentum"] > 0.0:
+                        torch.distributed.broadcast(state["ema"], src=0)
                     Q, L = state["QL"]
                     for q in Q:
                         torch.distributed.broadcast(q, src=0)
@@ -123,7 +132,7 @@ if __name__ == "__main__":
     rank = int(os.environ.get("RANK", "0")) # rank!=local_rank if #(nodes)>1
     device = torch.device(f"cuda:{local_rank}")
     torch.cuda.set_device(device)
-    torch.distributed.init_process_group(backend="nccl") # use gloo for complex tensors; nccl does not support complex tensors! 
+    torch.distributed.init_process_group(backend="nccl") # use gloo for complex tensors; nccl does not support complex tensors 
 
     class ToyModel(torch.nn.Module):
         def __init__(self):
