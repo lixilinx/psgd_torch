f83a25e7d26ae6f80be9a1325b00e032967cfda9
lixilinx <37510137+lixilinx@users.noreply.github.com>
2025-12-13 20:59:14 -0800

    torch.optim wrapping for DDP and FSDP
    1, the wrapping for DDP is almost the same, only set the default preconditioner dtype to bf16. 
    
    2, another wrapping is for DTensor and works for FSDP, TP, etc. I only precondition each slice of DTensor independently (thus not optimal). Still not clear what is a good way to precondition the whole DTensor.

--- wrapped_as_torch_optimizer_for_dtensor.py
+++ wrapped_as_torch_optimizer_for_dtensor.py
@@ -7,14 +7,15 @@ class WhitenMomentumNS4(torch.optim.Optimizer):
     """
     Whiten momentum with online Newton-Schulz iteration for inverse 4th root of momentum correlation matrix. 
     Largely corresponds to psgd.KronWhiten with dQ="Q0.5EQ1.5" and whiten_grad=False. 
-    Wrapped for single-GPU and multi-GPU DDP training. 
+    Wrapped for distributed training, say FSDP2, that uses Pytorch DTensor. 
+    This wrapping preconditions each slices of a DTensor gradient independently, not an optimal but generally acceptable solution.  
     """
 
     def __init__(
             self,
             params,
             preconditioner_max_size=float("inf"), 
-            preconditioner_max_skew=1.0, # 0.0 => all diagonal Q; inf => all dense Q
+            preconditioner_max_skew=8.0, # 0.0 => all diagonal Q; inf => all dense Q
             preconditioner_init_scale=1.0, # P0 = preconditioner_init_scale^2 * I; smaller is safer
             lr_params=3e-4, # roughly sqrt((1+momentum)/(1-momentum)) times smaller than Adam, i.e., sqrt((1-momentum)/(1+momentum)) * 1e-3   
             lr_preconditioner=0.1, # range (0, 1); don't anneal it to 0.01 for bfloat16 preconditioner as eps(bf16) ~ 0.01    
@@ -25,7 +26,7 @@ class WhitenMomentumNS4(torch.optim.Optimizer):
             grad_clip_max_amp=2.0, # rare to have amplitude>>1.0 after whitening; clip if too big 
             preconditioner_update_probability=0.1, # recommend to start from 1.0, and quickly anneal to 0.1 or 0.01 
             preconditioner_dtype:torch.dtype|None=torch.bfloat16, # bf16 should be good enough for most problems
-            resync_every=1000_000_000, # resync every # steps if nondeterminstic matmul diverges states too much; generally no need to resync   
+            resync_every=1000_000_000, # resync every # steps if states divergence too much due to nondeterminstic matmul; NOT implemented and generally no need   
     ):
         defaults = {
             "preconditioner_max_size": preconditioner_max_size, 
@@ -48,23 +49,21 @@ class WhitenMomentumNS4(torch.optim.Optimizer):
         self.update_precond = psgd.update_precond_kron_whiten_q0p5eq1p5
         self.precond_grad = psgd.precond_grad_kron
 
-        self.is_distributed = torch.distributed.is_available() and torch.distributed.is_initialized()
-        if self.is_distributed: # if True, assume multi-GPU DDP training; important to sync the rng states
-            state = torch.get_rng_state().cuda() # assume nccl backend
-            torch.distributed.broadcast(state, src=0)
-            self.cpu_rng_state = state.cpu()
+        # sync rng states 
+        state = torch.get_rng_state().cuda() # assume nccl backend
+        torch.distributed.broadcast(state, src=0)
+        self.cpu_rng_state = state.cpu()
 
-            state = torch.cuda.get_rng_state().cuda()
-            torch.distributed.broadcast(state, src=0)
-            self.cuda_rng_state = state.cpu()
+        state = torch.cuda.get_rng_state().cuda()
+        torch.distributed.broadcast(state, src=0)
+        self.cuda_rng_state = state.cpu()
 
     @torch.no_grad()
     def step(self):
-        if self.is_distributed: # sync internal rng states; save external rng states 
-            external_cpu_rng_state = torch.get_rng_state()
-            external_cuda_rng_state = torch.cuda.get_rng_state()
-            torch.set_rng_state(self.cpu_rng_state)
-            torch.cuda.set_rng_state(self.cuda_rng_state)
+        external_cpu_rng_state = torch.get_rng_state()
+        external_cuda_rng_state = torch.cuda.get_rng_state()
+        torch.set_rng_state(self.cpu_rng_state)
+        torch.cuda.set_rng_state(self.cuda_rng_state)
 
         for group in self.param_groups:
             update_preconditioner = torch.rand([]) < group["preconditioner_update_probability"]
@@ -79,9 +78,9 @@ class WhitenMomentumNS4(torch.optim.Optimizer):
 
                 preconditioner_dtype = group["preconditioner_dtype"]
                 if preconditioner_dtype:
-                    grad = grad.squeeze().to(preconditioner_dtype) # squeeze out dim=1; also good to merge small dims if possible
+                    grad = grad.to_local().squeeze().to(preconditioner_dtype) # squeeze out dim=1; also good to merge small dims if possible
                 else:
-                    grad = grad.squeeze()
+                    grad = grad.to_local().squeeze()
 
                 state = self.state[p]
                 if len(state) == 0: # intialization
@@ -104,36 +103,30 @@ class WhitenMomentumNS4(torch.optim.Optimizer):
                     g = grad
                 state["step"] += 1
 
-                if update_preconditioner:
+                if update_preconditioner and grad.numel() > 0: # no need to update preconditioner for empty sharded slices 
                     self.update_precond(state["QL"], state["exprs"], g, 
                                         lr=group["lr_preconditioner"], betaL=group["betaL"], damping=group["damping"])
                     
                 h = self.precond_grad(state["QL"], state["exprs"], g) # preconditioned momentum 
                 amp = torch.sqrt(torch.real(torch.mean(h * h.conj()))) # psgd supports complex tensors natively 
-                p.subtract_(group["lr_params"]/torch.clamp(amp/group["grad_clip_max_amp"], min=1.0) * h.view_as(p))
+                local_p = p.to_local()
+                local_p.subtract_(group["lr_params"]/torch.clamp(amp/group["grad_clip_max_amp"], min=1.0) * h.view_as(local_p))
 
-                # resync states occasionally if matmul is not determinstic and state divergence is large 
-                if self.is_distributed and (state["step"] % group["resync_every"] == 0):
-                    torch.distributed.broadcast(p, src=0)
-                    if group["momentum"] > 0.0:
-                        torch.distributed.broadcast(state["ema"], src=0)
-                    Q, L = state["QL"]
-                    for q in Q:
-                        torch.distributed.broadcast(q, src=0)
-                    for ell in L:
-                        torch.distributed.broadcast(ell, src=0)
+                # resync states occasionally if matmul is not determinstic and divergence of replicated state is large 
+                if state["step"] % group["resync_every"] == 0:
+                    pass # check p.device_mesh and p.placements for groups with duplicated states and resync them as DDP wrapping if needed.     
 
-        if self.is_distributed: # save internal rng states; recover external rng states 
-            self.cpu_rng_state = torch.get_rng_state()
-            self.cuda_rng_state = torch.cuda.get_rng_state()
-            torch.set_rng_state(external_cpu_rng_state)
-            torch.cuda.set_rng_state(external_cuda_rng_state)
+        self.cpu_rng_state = torch.get_rng_state()
+        self.cuda_rng_state = torch.cuda.get_rng_state()
+        torch.set_rng_state(external_cpu_rng_state)
+        torch.cuda.set_rng_state(external_cuda_rng_state)
 
 
 if __name__ == "__main__":
     # toy demo for verification  
     import os
-    from torch.nn.parallel import DistributedDataParallel as DDP
+    from torch.distributed.device_mesh import init_device_mesh
+    from torch.distributed.fsdp import fully_shard as FSDP
 
     local_rank = int(os.environ.get("LOCAL_RANK", "0"))
     rank = int(os.environ.get("RANK", "0")) 
@@ -144,21 +137,31 @@ if __name__ == "__main__":
     class ToyModel(torch.nn.Module):
         def __init__(self):
             super(ToyModel, self).__init__()
-            self.w = torch.nn.Parameter(torch.randn(1,2,3,4))
+            self.w = torch.nn.Parameter(torch.empty(4, 2))
 
         def forward(self, x):
             y = (self.w - torch.rand_like(self.w)) * x
-            return torch.real(torch.mean(y * y.conj()))
+            return torch.mean(y * y)
 
-    model = DDP(ToyModel().to(device), device_ids=[local_rank])
+    # test with 2 x 2 = 4 GPUs
+    mesh_2d = init_device_mesh("cuda", (2, 2), mesh_dim_names=("dp_replicate", "dp_shard"))
+
+    with torch.device("meta"):
+        model = ToyModel()
+    model.to_empty(device="cuda")
+    FSDP(model, mesh=mesh_2d)
     opt = WhitenMomentumNS4(model.parameters())
     for _ in range(1000):
         opt.zero_grad()
-        loss = model(torch.randn_like(model.module.w))
+        loss = model(torch.randn_like(model.w.full_tensor()))
         loss.backward()
         opt.step()
 
-    # let's verify the wrapping  
+    # let's verify the wrapping.
+    # pairs with the same local weights and preconditioner: (rank 0, rank 2) and (rank 1, rank 3). 
+    # pairs with different local weights and preconditioner: (rank 0, rank 1) and (rank 2, rank 3).
+    # full weights should be the same for all ranks.    
     print(f"rank {rank} got loss {loss}") # different GPU should have different loss 
-    print(f"rank {rank} got model weights {model.module.w}") # model weights should be close (no bitwise match guarantee in general)
-    print(f"""rank {rank} got preconditioner states {opt.state[model.module.w]["QL"]}""") # close but no bitwise match guarantee
+    print(f"rank {rank} got local model weights {model.w.to_local()}") 
+    print(f"rank {rank} got full model weights {model.w.full_tensor()}") 
+    print(f"""rank {rank} got local preconditioner states {opt.state[model.w]["QL"]}""") 
